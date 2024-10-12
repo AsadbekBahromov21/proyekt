93535c09f50c51dfd12b4d0d13a35f2678405f12
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-10-12 14:53:24 +0500

    redux

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "../redux/slice/AuthSlice";
 import counterSlice from "./slice/counter-slice";
 import { api } from "./api";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    // auth: authReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
