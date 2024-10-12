5b374e5313d66099a1f993e62b78707d4415f9f2
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-10-12 14:37:42 +0500

    proekt

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div>
+        <h2>Header</h2>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/singUp"}>SingUp</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+    </div>
+  )
+}
+
+export default Header
